0db9070482c9ef8b0e51876fe1c996516fa11142
Stephen <stephenhardiman96@gmail.com>
2020-06-27 11:21:55 +1000

    add build

---
+++ build/precache-manifest.87df0e05b0706587693d397e79d7ef68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc7a20915e88b92b013856d7d864921",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54245460bd9e396b13a4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4e076e94eed5a1f5eb1",
+    "url": "/static/js/2.aa9ee3a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa9ee3a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54245460bd9e396b13a4",
+    "url": "/static/js/main.8a2fdcab.chunk.js"
+  },
+  {
+    "revision": "0ae15a8d6fb37ec3b332",
+    "url": "/static/js/runtime-main.4ee4521d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
