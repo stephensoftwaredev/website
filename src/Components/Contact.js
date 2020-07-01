28a848a88e0ac5a2f1e64b197004ef4044ae8c2b
Stephen <stephenhardiman96@gmail.com>
2020-07-01 16:02:27 +1000

    aa

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -84,11 +84,6 @@ class Contact extends Component {
 						   <span>{phone}</span>
                      <span>{email}</span>
 					   </p>
-                 
-                  <ul className="social-links">
-              {networks}
-              
-           </ul>
 				   </div>
 
                <div className="widget widget_tweets">
