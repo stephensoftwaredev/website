c080d55fac6a20d2650fdb682a2bdca0a278be01
Stephen <stephenhardiman96@gmail.com>
2020-07-01 15:46:31 +1000

    email footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -16,12 +16,9 @@ class Footer extends Component {
         <div className="twelve columns">
            <ul className="social-links">
               {networks}
+              
            </ul>
-
-           <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
+           
 
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
