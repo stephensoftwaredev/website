01f90852f563b4212aa78abe8b78ca076f3a3bfb
Stephen <stephenhardiman96@gmail.com>
2020-07-01 18:16:47 +1000

    sss

--- src/Components/About.js
+++ src/Components/About.js
@@ -37,7 +37,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                  <a href={resumeDownload} className="button"><i src="s3://pdfbucketsstephen/CV.pdfCV.pdf"></i>Download Resume</a>
+                  <a href={resumeDownload} className="button"><i src=""></i>Download Resume</a>
                   </p>
                </div>
             </div>
