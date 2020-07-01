2f4630cdd6eb6ebc97d33c802133692d92f12dd5
Stephen <stephenhardiman96@gmail.com>
2020-07-01 13:02:51 +1000

    CV

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -38,7 +38,7 @@ class Header extends Component {
             <h3>{description}.</h3>
             <hr />
             <ul className="social">
-               <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
+               <a href={CV} className="button btn CV-btn"><i className="fa fa-book"></i>CV</a>
                <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
             </ul>
          </div>
