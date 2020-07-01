452b1954f0a5e40a4580bcd8bae5bd556e0ecf16
Stephen <stephenhardiman96@gmail.com>
2020-07-01 15:34:56 +1000

    contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -78,12 +78,11 @@ class Contact extends Component {
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
+					   <h4>Contact Me</h4>
 					   <p className="address">
 						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
 						   <span>{phone}</span>
+                     <span>{email}</span>
 					   </p>
 				   </div>
 
